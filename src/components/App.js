5511c5f7b2aeb76146768ebe9a211509c31216b5
nkusumo <nis.kusumo@gmail.com>
2021-06-14 12:28:17 -0700

    commit tree b8b3ea4a84e1eeb7ef1da3fef262f70254e618a6

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [eatenSushi, setEatenSushi] = useState([]);
+  const [money, setMoney] = useState(100)
+
+  function handleEatenSushi(price) {
+    let newArray = [...eatenSushi, price]
+    setEatenSushi(newArray)
+    setMoney(100 - newArray.reduce((a, b) => a + b, 0))
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={API} handleEatenSushi={handleEatenSushi} moneyRemaining={money} />
+      <Table plates={eatenSushi} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({sushis, handleEatenSushi, moneyRemaining}) {
+
+  const [sushiArray, setSushiArray] = useState([]);
+  const [startIndex, setStartIndex] = useState(0)
+  const [endIndex, setEndIndex] = useState(4)
+
+  useEffect(() => {
+    fetch(sushis)
+    .then(res => res.json())
+    .then(data => setSushiArray(data))
+  },[])
+
+  const sushiDiv = sushiArray.slice(startIndex,endIndex).map(sushi => <Sushi key={sushi.id} id={sushi.id} name={sushi.name} image={sushi["img_url"]} price={sushi.price} eatenSushi={handleEatenSushi} moneyRemaining={moneyRemaining} />)
+
+  function moreClick(e) {
+    let newStartIndex = startIndex + 4;
+    let newEndIndex = endIndex + 4;
+    setStartIndex(newStartIndex);
+    setEndIndex(newEndIndex);
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiDiv}
+      <MoreButton moreClick={moreClick} />
     </div>
   );
 }
